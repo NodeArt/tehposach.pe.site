0354e08064a7c4608eb54eb75aca92d44214ac67
dnesterenko27 <d.nesterenko27@gmail.com>
2018-04-06 22:26:17 +0300

    Form verif added

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@
                 name: form.elements.name.value,
                 email: form.elements.email.value,
                 subject: form.elements.subject.value,
-                message: form.elements.textarea.value
+                message: ( (form.elements.textarea) ? form.elements.textarea.value : 'no message sent' )
             };
             console.log(JSON.stringify(data));
             fetch('/sendMail', {

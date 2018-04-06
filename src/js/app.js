10971d2282932fbb6b3771fbceef84dba64993f6
dnesterenko27 <d.nesterenko27@gmail.com>
2018-04-06 21:52:47 +0300

    Functions

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    const form = document.getElementById('inputForm');
+
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            const data = {
+                name: form.elements.name.value,
+                email: form.elements.email.value,
+                subject: form.elements.subject.value,
+                message: form.elements.textarea.value
+            };
+            console.log(JSON.stringify(data));
+            fetch('/sendMail', {
+                method: "POST",
+                body: JSON.stringify(data)
+            })
+                .then(res => res.text())
+                .then(data => console.log(data))
+                .catch(e => console.log(e));
+        });
+    }
+})();
